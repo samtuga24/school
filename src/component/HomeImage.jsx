bd75dc5af6967e28f548b9fd0dc1de536cdd54c8
Samuel Owusu <owusu684@gmail.com>
2024-09-10 13:28:55 +0000

    first comment

--- src/component/HomeImage.jsx
+++ src/component/HomeImage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
-
+import home from '../icons/Adu.jpg'
 export const HomeImage = () => {
   return (
-    <div className='home-image-container'></div>
+    <div className='home-image-container'><img src={home} alt="" /></div>
   )
 }
