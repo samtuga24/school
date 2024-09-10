c97db268b5c46a6822325b824d237ec644ba68aa
Samuel Owusu <owusu684@gmail.com>
2024-09-10 13:55:23 +0000

    first comment

--- src/component/ViewApplicants.jsx
+++ src/component/ViewApplicants.jsx
@@ -3,9 +3,9 @@ import React, { useEffect, useState } from 'react'
 import { Document } from 'react-pdf';
 
 export const ViewApplicants = () => {
-    const [jobs, setJobs] = useState([])
+    const [apply, setJobs] = useState([])
     const [update, setUpdate] = useState([])
-    console.log(jobs)
+
     const GET_JOBS_API = "http://localhost:8080/get-applicants"
     const VIEW_CV = "http://localhost:8080/display/"
     useEffect(() => {
@@ -16,9 +16,8 @@ export const ViewApplicants = () => {
                 console.log(error)
             })
     }, []);
-
     const rowClick = (cv) => {
-        console.log(jobs)
+       
         axios.get(`http://localhost:8080/display/${cv}`)
             .then(response => {
                 // <Document file={`http://localhost:8080/display/${cv}`}/>
@@ -36,14 +35,16 @@ export const ViewApplicants = () => {
                         <th>Last Name</th>
                         <th>Email</th>
                         <th>Phone Number</th>
+                        <th>Job Title</th>
                     </tr>
-                    {jobs.map((job, index) => {
+                    {apply.map((job, index) => {
                         return (
                             <tr className='table-row' onClick={() => rowClick(job.applicantCV.name)}>
                                 <td className='t-data'>{job.fname}</td>
                                 <td className='t-data'>{job.lname}</td>
                                 <td className='t-data'>{job.uname}</td>
                                 <td className='t-data'>{job.phoneNumber}</td>
+                                <td className='t-data'>{job.jobs[index].title}</td>
                             </tr>
                         );
                     })}
