e3119813b05f0b1412516421ee746e805fab4bf6
Samuel Owusu <owusu684@gmail.com>
2024-09-13 12:51:10 +0000

    first comment

--- src/component/UserPage.jsx
+++ src/component/UserPage.jsx
@@ -139,9 +139,9 @@ export const UserPage = () => {
                                                 <div className='result-label'>Job Title:</div>
                                                 <div className='result-text'>{job.title}</div>
                                             </div>
-                                            <div className='result'>
+                                            <div className='desc'>
                                                 <div className='result-label'>Description:</div>
-                                                <div className='result-text'>{job.description}</div>
+                                                <div className='desc-text'>bhhbhsdubuy suysuyvusvuysv uysvyddddjjjj jjjjjjjjjjjj jjjjjjjjjjjjjjjjjjjjj jjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj</div>
                                             </div>
                                             <div className='result'>
                                                 <div className='result-label'>Qualification:</div>
