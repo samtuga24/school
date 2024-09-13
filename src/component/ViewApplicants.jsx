724f075c303fa9aea75bc893e189be21166d9783
Samuel Owusu <owusu684@gmail.com>
2024-09-13 08:22:06 +0000

    first comment

--- src/component/ViewApplicants.jsx
+++ src/component/ViewApplicants.jsx
@@ -19,7 +19,7 @@ export const ViewApplicants = () => {
     }, []);
 
     const rowClick = (cv) => {
-       
+
         axios.get(`http://localhost:8080/display/${cv}`)
             .then(response => {
                 // <Document file={`http://localhost:8080/display/${cv}`}/>
@@ -29,19 +29,19 @@ export const ViewApplicants = () => {
             })
     }
 
-    const updateStatus = (e,email) =>{
+    const updateStatus = (e, email) => {
         e.stopPropagation();
         axios
-          .patch(`http://localhost:8080/update-status/${email}`, {
+            .patch(`http://localhost:8080/update-status/${email}`, {
 
-          })
-          .then(response => {
-            alert(JSON.stringify(response.data))
+            })
+            .then(response => {
+                alert(JSON.stringify(response.data))
 
 
-          }).catch((error) => {
-            alert(error)
-          })
+            }).catch((error) => {
+                alert(error)
+            })
     }
     return (
         <div className='view-container'>
@@ -57,21 +57,45 @@ export const ViewApplicants = () => {
                         <th>Actions</th>
                     </tr>
                     {apply.map((job, index) => {
-                        return (
-                            <tr className='table-row' onClick={() => rowClick(job.applicantCV.name)}>
-                                <td className='t-data'>{job.fname}</td>
-                                <td className='t-data'>{job.lname}</td>
-                                <td className='t-data'>{job.uname}</td>
-                                <td className='t-data'>{job.phoneNumber}</td>
-                                <td className='t-data'>{job.jobs[index].title}</td>
-                                <td className='t-data'>{job.jobStatus}</td>
-                                <td className='t-data action-btn' onClick={(e)=>updateStatus(e,job.uname)}>
-                                    <div className='accept-btn'><div className='accept-icon'><FontAwesomeIcon icon={faCheck}/></div><div className='accept-text'>Accept</div></div>
-                                </td>
-                            </tr>
-                        );
-                    })}
 
+                        return (
+                            <>
+                                {job.jobs.map((details, i) => {
+                                    return (
+                                        <tr className='table-row' onClick={() => rowClick(job.applicantCV.name)}>
+                                            <td className='t-data'>{job.fname}</td>
+                                            <td className='t-data'>{job.lname}</td>
+                                            <td className='t-data'>{job.uname}</td>
+                                            <td className='t-data'>{job.phoneNumber}</td>
+                                            <td className='t-data'>{job.jobs[index].title}</td>
+                                            <td className='t-data'>{job.jobStatus}</td>
+                                            <td className='t-data action-btn' onClick={(e) => updateStatus(e, job.uname)}>
+                                                <div className='accept-btn'><div className='accept-icon'><FontAwesomeIcon icon={faCheck} /></div><div className='accept-text'>Accept</div></div>
+                                            </td>
+                                        </tr>
+                                    );
+
+                                })}
+                            </>
+                        );
+                        // return (
+                        //     <></>
+                        //     {job.jobs.map((details, i)=>{
+
+                        //     })}
+                        //     <tr className='table-row' onClick={() => rowClick(job.applicantCV.name)}>
+                        //         <td className='t-data'>{job.fname}</td>
+                        //         <td className='t-data'>{job.lname}</td>
+                        //         <td className='t-data'>{job.uname}</td>
+                        //         <td className='t-data'>{job.phoneNumber}</td>
+                        //         <td className='t-data'>{job.jobs[index].title}</td>
+                        //         <td className='t-data'>{job.jobStatus}</td>
+                        //         <td className='t-data action-btn' onClick={(e)=>updateStatus(e,job.uname)}>
+                        //             <div className='accept-btn'><div className='accept-icon'><FontAwesomeIcon icon={faCheck}/></div><div className='accept-text'>Accept</div></div>
+                        //         </td>
+                        //     </tr>
+                        // );
+                    })}
                 </table>
             </div>
         </div>
