e64c0cf9cc577c40badd27cbf5da5432f87a74e3
Samuel Owusu <owusu684@gmail.com>
2024-09-26 13:10:39 +0000

    first commit

--- src/component/EditProfile.jsx
+++ src/component/EditProfile.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import { Button, Modal } from 'react-bootstrap';
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 export const EditProfile = (props) => {
   const [click, setClick] = useState(false)
   const [defaultClick, setDefault] = useState(true)
@@ -16,15 +17,25 @@ export const EditProfile = (props) => {
           <Modal.Title>Edit Profile</Modal.Title>
         </Modal.Header>
         <div className='modal-body-container'>
-          <div className='mdal-body-wrap'>
+          <div className='modal-body-wrap'>
             <div className='profile-image-wrap'>
-              <div className='profile-image'></div>
+              <div className='profile-image'><div className='default-image'><FontAwesomeIcon icon={faUser}/></div></div>
               <div className='edit-user'>Edit</div>
             </div>
             <form action="" className='modal-form'>
               <div>
                 <div className={click ? 'modal-default' : 'modal-login'} onMouseDown={clickDefault}>
                   <div className='modal-label'>First Name</div>
+                  <input className='modal-input' type="text" name="fname" id=""/>
+                </div>
+
+                <div className={click ? 'modal-default' : 'modal-login'} onMouseDown={clickDefault}>
+                  <div className='modal-label'>Last Name</div>
+                  <input className='modal-input' type="text" name="fname" id=""/>
+                </div>
+
+                <div className={click ? 'modal-default' : 'modal-login'} onMouseDown={clickDefault}>
+                  <div className='modal-label'>Username</div>
                   <input className='modal-input' type="text" name="fname" id=""/>
                 </div>
                 <div className='edit-button'><button className='l-bt-edit' type='submit'>Save changes</button></div>
