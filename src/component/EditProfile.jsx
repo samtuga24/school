65e703ecd83d3e2ae2253b1dfd6c1eee6beda94c
Samuel Owusu <owusu684@gmail.com>
2024-09-26 14:20:36 +0000

    first commit

--- src/component/EditProfile.jsx
+++ src/component/EditProfile.jsx
@@ -1,14 +1,41 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import { Button, Modal } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { faUser, faCamera } from '@fortawesome/free-solid-svg-icons';
 export const EditProfile = (props) => {
+  const getData = localStorage.getItem("user");
+  const parsedData = JSON.parse(getData);
+  const uploadRef = useRef()
   const [click, setClick] = useState(false)
+  const [second, setSecond] = useState(false)
+  const [third, setThird] = useState(false)
   const [defaultClick, setDefault] = useState(true)
   const [inputLength,  setLength] = useState(false)
+
+  console.log(parsedData)
   const clickDefault = () => {
-    setClick(!click)
-    setDefault(!defaultClick)
+    setClick(true)
+    setDefault(false)
+    setSecond(false)
+    setThird(false)
+  }
+
+  const clickSecond = () => {
+    setClick(false)
+    setDefault(false)
+    setSecond(true)
+    setThird(false)
+  }
+
+  const clickThird = () => {
+    setClick(false)
+    setDefault(false)
+    setSecond(false)
+    setThird(true)
+  }
+
+  const clickEdit = () =>{
+    uploadRef.current.click()
   }
   return (
     <>
@@ -20,23 +47,24 @@ export const EditProfile = (props) => {
           <div className='modal-body-wrap'>
             <div className='profile-image-wrap'>
               <div className='profile-image'><div className='default-image'><FontAwesomeIcon icon={faUser}/></div></div>
-              <div className='edit-user'>Edit</div>
+              <div className='edit-user' onClick={clickEdit}><FontAwesomeIcon icon={faCamera}/></div>
+              <div className='select-edit'><input ref={uploadRef} type="file" accept='image/*'/></div>
             </div>
             <form action="" className='modal-form'>
               <div>
                 <div className={click ? 'modal-default' : 'modal-login'} onMouseDown={clickDefault}>
                   <div className='modal-label'>First Name</div>
-                  <input className='modal-input' type="text" name="fname" id=""/>
+                  <input className='modal-input' type="text" name="fname" id="" defaultValue={parsedData.fname}/>
                 </div>
 
-                <div className={click ? 'modal-default' : 'modal-login'} onMouseDown={clickDefault}>
+                <div className={second ? 'modal-default' : 'modal-login'} onMouseDown={clickSecond}>
                   <div className='modal-label'>Last Name</div>
-                  <input className='modal-input' type="text" name="fname" id=""/>
+                  <input className='modal-input' type="text" name="lname" id="" defaultValue={parsedData.lname}/>
                 </div>
 
-                <div className={click ? 'modal-default' : 'modal-login'} onMouseDown={clickDefault}>
+                <div className={third ? 'modal-default' : 'modal-login'} onMouseDown={clickThird}>
                   <div className='modal-label'>Username</div>
-                  <input className='modal-input' type="text" name="fname" id=""/>
+                  <input className='modal-input' type="text" name="uname" id="" defaultValue={parsedData.uname}/>
                 </div>
                 <div className='edit-button'><button className='l-bt-edit' type='submit'>Save changes</button></div>
               </div>
